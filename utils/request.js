42798f81c56c5ae3a61a3fe39582bfb755b651da
‘xu’ <laike.xu@shengyc.com>
2021-04-13 18:14:16 +0800

    update

--- utils/request.js
+++ utils/request.js
@@ -1,10 +1,10 @@
 // // 配置axios
 
-// const request = axios.create({
-//   baseURL: 'http://apidev.sycdev.com:80', // url = base url + request url
-//   withCredentials: true, // send cookies when cross-domain requests
-//   timeout: 900000 // request timeout
-// })
+const request = axios.create({
+  baseURL: 'http://apidev.sycdev.com:80', // url = base url + request url
+  withCredentials: true, // send cookies when cross-domain requests
+  timeout: 900000 // request timeout
+})
 
 // // request interceptor
 // request.interceptors.request.use(
